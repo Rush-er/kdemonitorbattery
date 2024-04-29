70200f0a4cec1314d22f348531d08b2bc663d3d8
Matis <32925370+Rush-er@users.noreply.github.com>
2024-04-29 15:39:22 +0200

    Update change_display_battery.sh

--- change_display_battery.sh
+++ change_display_battery.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-while true
-do
-    if on_ac_power; then
-        kscreen-doctor output.83.mode.110               ## Laptop on power
+
+while true; do
+    status=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep "state:" | cut -d: -f 2)
+    if [[ "$status" == *"discharging"* ]]; then
+        kscreen-doctor output.2.mode.10    # Laptop on battery
     else
-        kscreen-doctor output.83.mode.96          ## Laptop on battery
+        kscreen-doctor output.2.mode.14    # Laptop on power
     fi
-    sleep 10                       ## wait 10 sec before repeating
+    sleep 10
 done
